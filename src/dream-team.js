4b3466e020e910e02d3be440a7b1e9385b19ad10
EvgeniaYaroslavich <yaroslavichjane@gmail.com>
2021-03-16 19:11:56 +0400

    feat: solution dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  
+  if (members === []) {
+    return false;
+  } else if (Array.isArray(members)) {
+    dreamTeam = [];
+    dreamTeamStr = "";
+    for (let i = 0; i < members.length; i++) {
+      if ((typeof members[i]) === "string") {
+        letter = members[i].trim().toUpperCase()[0];
+        dreamTeam.push(letter);
+      }
+    }
+    dreamTeam.sort();
+    dreamTeam.forEach((element) => {
+      dreamTeamStr += element;
+    });
+
+    // console.log(dreamTeamStr);
+    return dreamTeamStr;
+  } else {
+    return false;
+  }
 };
